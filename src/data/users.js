a4bedcad4fc38de663400addb634188d1310215b
Maro de Melo <marodemelo10@gmail.com>
2021-04-25 10:25:56 -0300

    Criados inputs para atualizacao dos campos de usuarios

--- src/data/users.js
+++ src/data/users.js
@@ -6,19 +6,19 @@ export default [
         avatarUrl: 'https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425_960_720.png'
     },
     {
-    id: 2,
+        id: 2,
         name: 'Ricardo Pereira',
         email: 'ririper@gmail.com',
         avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png'
     },
     {
-    id: 3,
+        id: 3,
         name: 'Mariana Aguiar',
         email: 'mariagui@gmail.com',
         avatarUrl: 'https://cdn.pixabay.com/photo/2014/04/02/14/10/female-306407_960_720.png'
     },
     {
-    id: 4,
+        id: 4,
         name: 'Rafael Fragoso',
         email: 'rafafra@gmail.com',
         avatarUrl: 'https://cdn.pixabay.com/photo/2018/08/28/13/29/avatar-3637561_960_720.png'
--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,9 +1,64 @@
-import React from 'react'
-import {Text} from 'react-native'
+import React, {useState} from 'react'
+import {Text, View,TextInput, StyleSheet, Button} from 'react-native'
 
-export default props => {
-    console.warn(Object.keys(props.route.params))
+
+export default ({route,navigation}) => {
+   
+   const [ user, setUser] = useState(route.params ? route.params : {})
     return (
-        <Text>UserForm</Text>
+        <>
+            <View 
+            style={style.form}>
+                <Text 
+                style={style.label}>Nome</Text>
+                <TextInput
+                style={style.input}
+                onChangeText={name=> setUser({...user, name})}
+                placeholder="Informe o nome"
+                value={user.name}
+                />
+                 <Text 
+                style={style.label}>Email</Text>
+                <TextInput
+                style={style.input}
+                onChangeText={email=> setUser({...user, email})}
+                placeholder="Informe o e-mail"
+                value={user.email}
+                />
+                 <Text 
+                style={style.label}>URL</Text>
+                <TextInput
+                style={style.input}
+                onChangeText={avatarUrl=> setUser({...user, avatarUrl})}
+                placeholder="Informe a URL"
+                value={user.avatarUrl}
+                />
+                <Button 
+                title="Salvar"
+                onPress= {() => {
+                    navigation.goBack()
+                }}
+                 />
+            </View>
+            
+           
+               
+           
+         </>
+        
     )
 }
+
+const style = StyleSheet.create({
+    form: {
+        padding: 12,
+    },
+    
+    input: {
+        height: 40,
+        borderColor: 'grey',
+        borderWidth: 1,
+        marginBottom: 15,
+        
+    }
+})
