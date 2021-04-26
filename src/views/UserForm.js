25b4995d05a4349dd78baa64bb8f8dcf44fafc7e
Maro de Melo <marodemelo10@gmail.com>
2021-04-26 18:49:12 -0300

    Criadas actions para criar o usuário e atualizar usuário

--- src/views/UserForm.js
+++ src/views/UserForm.js
@@ -1,10 +1,12 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
 import {Text, View,TextInput, StyleSheet, Button} from 'react-native'
+import UsersContext from '../context/UsersContext'
 
 
 export default ({route,navigation}) => {
    
    const [ user, setUser] = useState(route.params ? route.params : {})
+   const {dispatch} = useContext(UsersContext)
     return (
         <>
             <View 
@@ -36,6 +38,10 @@ export default ({route,navigation}) => {
                 <Button 
                 title="Salvar"
                 onPress= {() => {
+                    dispatch({
+                        type: user.id ? 'updateUser' : 'createUser',
+                        payload: user
+                    })
                     navigation.goBack()
                 }}
                  />
